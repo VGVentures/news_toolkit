a7eb22947c389aab6a402df7038afcf1584e4bb9
Josh-Sanford <josh.sanford@verygood.ventures>
2025-12-03 15:10:06 -0800

    replace firebase_dynamic_links with app_links

---
+++ flutter_news_example/packages/deep_link_client/app_links_deep_link_client/lib/src/app_links_deep_link_client.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
+import 'package:deep_link_client/deep_link_client.dart';
+
+/// {@template firebase_deep_link_client}
+/// An AppLinks implementation of [DeepLinkClient].
+/// {@endtemplate}
+class AppLinksDeepLinkClient implements DeepLinkClient {
+  /// {@macro app_links_deep_link_client}
+  AppLinksDeepLinkClient({AppLinks? appLinks})
+    : _appLinks = appLinks ?? AppLinks();
+
+  final AppLinks _appLinks;
+
+  @override
+  Stream<Uri> get deepLinkStream =>
+      _appLinks.uriLinkStream;
+
+  @override
+  Future<Uri?> getInitialLink() => _appLinks.getInitialLink();
+}
---
+++ flutter_news_example/packages/deep_link_client/app_links_deep_link_client/test/app_links_deep_link_client_test.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'package:app_links/app_links.dart';
+import 'package:app_links_deep_link_client/app_links_deep_link_client.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAppLinks extends Mock implements AppLinks {}
+
+void main() {
+  late MockAppLinks appLinks;
+  late StreamController<Uri> uriLinkStreamController;
+
+  setUp(() {
+    appLinks = MockAppLinks();
+    uriLinkStreamController = StreamController<Uri>();
+    when(
+      () => appLinks.uriLinkStream,
+    ).thenAnswer((_) => uriLinkStreamController.stream);
+  });
+
+  tearDown(() {
+    unawaited(uriLinkStreamController.close());
+  });
+
+  group('AppLinksDeepLinkClient', () {
+    group('getInitialLink', () {
+      test('retrieves the latest link if present', () async {
+        final expectedUri = Uri.https('ham.app.test', '/test/path');
+        when(appLinks.getInitialLink).thenAnswer(
+          (_) => Future.value(expectedUri),
+        );
+
+        final client = AppLinksDeepLinkClient(
+          appLinks: appLinks,
+        );
+        final link = await client.getInitialLink();
+        expect(link, expectedUri);
+      });
+    });
+
+    group('deepLinkStream', () {
+      test('publishes values received through uriLinkStream', () {
+        final expectedUri1 = Uri.https('news.app.test', '/test/1');
+        final expectedUri2 = Uri.https('news.app.test', '/test/2');
+
+        final client = AppLinksDeepLinkClient(
+          appLinks: appLinks,
+        );
+
+        uriLinkStreamController
+          ..add(expectedUri1)
+          ..add(expectedUri1)
+          ..add(expectedUri2)
+          ..add(expectedUri1);
+
+        expect(
+          client.deepLinkStream,
+          emitsInOrder(<Uri>[
+            expectedUri1,
+            expectedUri1,
+            expectedUri2,
+            expectedUri1,
+          ]),
+        );
+      });
+    });
+  });
+}
