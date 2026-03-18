0d48b22439d625cf488ab9c3d972f1108074c8be
Ricardo Dalarme <ricardo.dalarme@verygood.ventures>
2026-03-18 09:35:42 -0300

    fix: add workaround to avoid gradle issues related to twitter_login
    Reference:
    - https://github.com/0maru/twitter_login/issues/140

--- flutter_news_example/android/build.gradle.kts
+++ flutter_news_example/android/build.gradle.kts
@@ -19,6 +19,24 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Temporary workaround for third-party Android modules that still do not set
+// an explicit namespace. Remove once upstream dependencies are updated.
+// https://github.com/0maru/twitter_login/issues/140
+subprojects {
+    pluginManager.withPlugin("com.android.library") {
+        val androidExtension = extensions.findByName("android") ?: return@withPlugin
+        val androidClass = androidExtension.javaClass
+        val getNamespace = runCatching { androidClass.getMethod("getNamespace") }.getOrNull() ?: return@withPlugin
+        val setNamespace =
+            runCatching { androidClass.getMethod("setNamespace", String::class.java) }.getOrNull() ?: return@withPlugin
+
+        val namespace = getNamespace.invoke(androidExtension) as? String
+        if (namespace.isNullOrBlank()) {
+            setNamespace.invoke(androidExtension, project.group.toString())
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
